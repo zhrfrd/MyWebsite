703a18dcbb33bc6a2db21ea372b940b19a3b1e9e
zhrfrd <zouheirjob@hotmail.com>
2021-02-15 17:46:05 +0000

    Change from html to php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<?php 
+    echo '
+    <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+    <a href="#"><i class="fa fa-facebook-official">fd</i></a>
+    <a href="#"><i class="fa fa-pinterest-p">df</i></a>
+    <a href="#"><i class="fa fa-twitter">df</i></a>
+    <a href="#"><i class="fa fa-flickr">df</i></a>
+    <a href="#"><i class="fa fa-linkedin">df</i></a>
+    <img src="img_bandmember.jpg" alt="Random Name" style="width:100%">
+    <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>';
+?>
