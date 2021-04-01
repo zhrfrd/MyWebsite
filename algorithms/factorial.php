459e5589a1587355b4364d534a9b3ed54f15a523
zhrfrd <zouheirjob@hotmail.com>
2021-04-01 21:19:41 +0100

    Cleaned bubble sort code

--- algorithms/factorial.php
+++ algorithms/factorial.php
@@ -9,6 +9,9 @@
   
 <body>
     <br />
+    <input type="text" id="txtLength">
+    <button onclick="populateArray()">Populate</button>
+    <button onclick="bubbleSort()">Bubble Sort</button>
     <div id="arrayContainer"></div>
     <script src="../scripts/script.js"></script>
 </body>
--- scripts/script.js
+++ scripts/script.js
@@ -1,94 +1,96 @@
+var myArray = new Array();
 var container = document.getElementById("arrayContainer");
-  
-//Function to generate the array of blocks
-function generatearray() 
+var arrayLength;
+
+//Function to generate the array of divBlocks
+function populateArray() 
 {
-    for (var i = 0; i < 20; i++) 
+    arrayLength = parseInt(document.getElementById("txtLength").value);
+
+    for (var i = 0; i < arrayLength; i++) 
     {
-        //Return a value from 1 to 100 (both inclusive)
-        var value = Math.ceil(Math.random() * 100);
+        var value = Math.ceil(Math.random() * 100);   //Generate a random number from 1 to 100 inclusive
+        myArray[i] = value;   //Populate array
+        var arrayElementDiv = document.createElement("div");   //Create a div HTML element
+        arrayElementDiv.classList.add("divElement");   //Assign a class to the div HTML element
   
-        //Creating element div
-        var arrayElement = document.createElement("div");
+        //Create style to the div element
+        arrayElementDiv.style.height = `${value * 3}px`;   //Set height of the div
+        arrayElementDiv.style.transform = `translate(${i * 30}px)`;   //Set the position of the following div
   
-        // Adding class 'block' to div
-        arrayElement.classList.add("divElement");
-  
-        //Adding style to div
-        arrayElement.style.height = `${value * 3}px`;
-        arrayElement.style.transform = `translate(${i * 30}px)`;
-  
-        //Creating label element for displaying 
-        //size of particular block
-        var arrayElementLabel = document.createElement("label");
-        arrayElementLabel.classList.add("divElementStyle");
-        arrayElementLabel.innerText = value;
-  
-        //Appending created elements to index.html 
-        arrayElement.appendChild(arrayElementLabel);
-        container.appendChild(arrayElement);
+        //Create the labels to show the value of the div
+        var arrayElementDivLabel = document.createElement("label");
+        arrayElementDivLabel.classList.add("divElementStyle");
+        arrayElementDivLabel.textContent = value;
+
+        arrayElementDiv.appendChild(arrayElementDivLabel);   
+        container.appendChild(arrayElementDiv);
     }
 }
+
+//Asynchronous BubbleSort function (Accepts await expressions)
+async function bubbleSort() 
+{
+    var divBlocks = document.querySelectorAll(".divElement");   //Get all the elements in the document with class "divElement" and 
   
-//Promise to swap the two element of the array
-function swap(el1, el2) 
+    //Scan through the array
+    for (var i = 0; i < arrayLength; i ++)
+    {
+        for (var j = 0; j < arrayLength - i - 1; j ++)   
+        {
+            //Highlight the two divBlocks that are compared 
+            divBlocks[j].style.backgroundColor = "red";
+            divBlocks[j + 1].style.backgroundColor = "red";
+  
+            var value1 = Number(divBlocks[j].childNodes[0].textContent);       //Get the value of the block label, save it as a number ad assign it to the variable
+            var value2 = Number(divBlocks[j + 1].childNodes[0].textContent);   //
+  
+            //Compare the value of the two div blocks
+            if (value1 > value2) 
+            {
+                await swap(divBlocks[j], divBlocks[j + 1], myArray[j], myArray[j + 1]);   //Await "stops" the normal execution flow to run the function (swap). await needs to return the Promise result
+                divBlocks = document.querySelectorAll(".divElement");   //Update divBlocks after swapping
+            }
+  
+            //Changing the color to the previous one
+            divBlocks[j].style.backgroundColor = "purple";
+            divBlocks[j + 1].style.backgroundColor = "purple";
+        }
+
+        //Change the color of the last element to green decrementally 
+        divBlocks[arrayLength - i - 1].style.backgroundColor = "green";
+    }
+
+    alert(myArray);
+}
+
+//Promise to swap the two elements
+function swap(div1, div2, arrElement1, arrElement2) 
 {
     return new Promise((resolve) => 
     {
-        //For exchanging styles of two blocks
-        var temp = el1.style.transform;
-        el1.style.transform = el2.style.transform;
-        el2.style.transform = temp;
-  
+        //For exchanging styles of two divBlocks
+        var tempDiv;
+        var tempArr;
+
+        //Swap between array elements
+        tempArr = arrElement1;
+        arrElement1 = arrElement2;
+        arrElement2 = tempArr;
+
+        //Swap between divs
+        tempDiv = div1.style.transform;
+        div1.style.transform = div2.style.transform;
+        div2.style.transform = tempDiv;
+
         window.requestAnimationFrame(function() 
         {
             //For waiting for .25 sec
             setTimeout(() => 
             {
-                container.insertBefore(el2, el1);
+                container.insertBefore(div2, div1);
                 resolve();
             }, 500);
         });
     });
 }
-  
-//Asynchronous BubbleSort function (Accepts await expressions)
-async function BubbleSort() 
-{
-    var blocks = document.querySelectorAll(".divElement");   //Get all the elements in the document with class "divElement" and 
-  
-    //Scan through the array
-    for (var i = 0; i < blocks.length; i ++) 
-    {
-        for (var j = 0; j < blocks.length - i - 1; j ++)
-        {
-            //Highlight the two blocks that are compared 
-            blocks[j].style.backgroundColor = "#FF4949";
-            blocks[j + 1].style.backgroundColor = "#FF4949";
-  
-            var value1 = Number(blocks[j].childNodes[0].textContent);       //Get the value of the block label, save it as a number ad assign it to the variable
-            var value2 = Number(blocks[j + 1].childNodes[0].textContent);   //
-  
-            // To compare value of two blocks
-            if (value1 > value2) 
-            {
-                await swap(blocks[j], blocks[j + 1]);   //Await "stops" the normal execution flow to run the function (swap). await needs to return the Promise result
-                blocks = document.querySelectorAll(".divElement");
-            }
-  
-            //Changing the color to the previous one
-            blocks[j].style.backgroundColor = "#6b5b95";
-            blocks[j + 1].style.backgroundColor = "#6b5b95";
-        }
-  
-        //changing the color of greatest element 
-        //found in the above traversal
-        blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
-    }
-}
-  
-//Calling generatearray function
-generatearray();
-  
-//Calling BubbleSort function
-BubbleSort();
